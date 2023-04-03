8f788bebdd8e539854b55e0e9112b7070ad6d1a8
James Lunt <50912137+james-lunt@users.noreply.github.com>
2023-04-03 12:35:15 +0100

    Emulation

---
+++ network_emulation/introduction.py
@@ -0,0 +1,4 @@
+import time
+def introduce(name,region,delay):
+    print("Hello! Welcome to "+name+" in the "+region+" region!")
+    time.sleep(int(delay))
---
+++ network_emulation/purchase_add.py
@@ -0,0 +1,17 @@
+import argparse
+from introduction import introduce
+
+if __name__ =="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--shop', type=str)
+    parser.add_argument('--region', type=str)
+    parser.add_argument('--id', type=str)
+    parser.add_argument('--order', type=str)
+    parser.add_argument('--delay', type=str)
+    args = parser.parse_args()
+    
+    introduce(args.shop,args.region,args.delay)
+
+    print("Here's a " + args.order)
+    print("Adding points for your purchase...")
+    print("Updating transaction history")
