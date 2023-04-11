4b0b1027f8d5f76336f9256bec9c6f0630b091e6
Avinash S <sylendra@tcd.ie>
2023-04-11 15:16:23 +0100

    Change get user

--- api/users.py
+++ api/users.py
@@ -2,7 +2,7 @@ from latterouter import app
 from flask import request, jsonify
 from database.models import add_user, update_user, update_user_points, get_user_points, get_user
 
-@app.route("/users/<int:id>", methods=['GET', 'POST', 'PUT'])
+@app.route("/users/<int:id>", methods=['POST', 'PUT'])
 def user(id):
     if request.method == 'POST':
         # Adding new user
@@ -17,12 +17,6 @@ def user(id):
             response.status_code = 404
             return response
     
-    elif request.method == 'GET':
-        # Get user from id
-        response = jsonify(get_user(id))
-        response.status_code = 200
-        return response
-    
     else:
         # Update user
         data = request.get_json()
@@ -35,6 +29,12 @@ def user(id):
             response = jsonify({'error': 'Error adding user'})
             response.status_code = 500
             return response
+
+@app.route("/user/<string:userid>", methods=["GET"])
+def get_user(userid):
+    response = jsonify(get_user(userid))
+    response.status_code = 200
+    return response
 
 @app.route("/user/<int:id>/points", methods=["GET", 'PUT'])
 def user_points(id):
