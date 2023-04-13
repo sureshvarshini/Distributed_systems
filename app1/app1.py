a39163203c6a7fa7e4625a9f8f93bd4ac13bbc1d
stefanTogan <63917478+stefanTogan@users.noreply.github.com>
2023-04-13 12:50:23 +0100

    Load balancer new implementation attemp

---
+++ app1/app1.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app1 = Flask(__name__)
+
+
+@app1.route('/')
+def hello_world():
+    return 'Salam alikom, this is App1 :) '
+
+
+if __name__ == '__main__':
+   app1.run(debug=True, host='0.0.0.0')
+
+
