134ac87e3b2dec347d9968b7c18718392e1cc1d8
sureshvarshini <sureshv@tcd.ie>
2023-04-13 20:22:56 +0100

    Minor changes to re direct url to other region servers

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ REDIS_ADDRESS = config[REGION]['redis_address']
 def redirect_request_to_region(request_method:str, url:str, region_code:str, payload:dict = None):
     new_api_port = config[region_code]['api_port']
     new_url = url.replace(str(API_PORT), str(new_api_port))
-    new_url = new_url.replace('localhost', 'app1' + region_code.lower())
+    new_url = new_url.replace('localhost', region_code.lower() + '-app')
     print(new_url, flush=True)
     if request_method == 'POST':
         requests.post(new_url, json=payload)
